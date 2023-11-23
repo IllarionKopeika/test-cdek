d4d6b8bcaa92a0ac9ad4670e6c598f3e9b9f25ca
Illarion Kopeika <larrypearson@yandex.ru>
2023-11-23 16:18:32 +0800

    add CRUD actions

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,22 +1,46 @@
 class OrdersController < ApplicationController
+  before_action :set_restaurant, only: [:show, :edit, :update, :destroy]
+
   def index
+    @orders = Order.all
   end
 
   def new
+    @order = Order.new
   end
 
   def show
   end
 
   def create
+    @order = Order.new(params[:order])
+    @order.save
+
+    redirect_to order_path(@order)
   end
 
   def edit
   end
 
   def update
+    @order.update(order_params)
+
+    redirect_to order_path(@order)
   end
 
   def destroy
+    @order.destroy
+
+    redirect_to orders_path, status: :see_other
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:number)
+  end
+
+  def set_order
+    @order = Order.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,5 @@
 Rails.application.routes.draw do
-  get 'orders/index'
-  get 'orders/new'
-  get 'orders/show'
-  get 'orders/create'
-  get 'orders/edit'
-  get 'orders/update'
-  get 'orders/destroy'
+  resources :orders, only: [:index, :new, :create, :show, :edit, :update, :destroy]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
